66fe85aca916da08a0ce4718bec9c15b2577fee9
Megan McCarty <megan_mccarty@hotmail.com>
2021-05-21 10:17:17 -0400

    Add functions for -/+, timer, and pause

---
+++ js/challenge.js
@@ -0,0 +1,43 @@
+// Variables to hold existing HTML elements
+const counterContainer = document.getElementById('counter');
+let counter = parseInt(counterContainer.textContent, 10);
+const minusButton = document.getElementById('minus');
+const plusButton = document.getElementById('plus');
+const likeButton = document.getElementById('heart');
+const pauseButton = document.getElementById('pause');
+const likesList = document.querySelector('ul.likes');
+const commentsList = document.getElementById('list');
+const commentForm = document.querySelector('form#comment-form');
+
+// Function for counter to increment once per second
+let timer = setInterval(increment, 1000);
+
+// Event listeners for minus, plus, and pause buttons
+minusButton.addEventListener('click', decrement);
+plusButton.addEventListener('click', increment);
+pauseButton.addEventListener('click', pause);
+
+// Decrement function
+function decrement() {
+    counter -= 1;
+    return counterContainer.textContent = counter;
+}
+
+// Increment function
+function increment() {
+    counter += 1;
+    return counterContainer.textContent = counter
+}
+
+// Pause function
+function pause() {
+
+    // Pauses/resumes timer depending on the text displayed in the pause button
+    if (pauseButton.textContent === ' pause ') {
+        pauseButton.textContent = ' resume ';
+        clearInterval(timer);
+    } else if (pauseButton.textContent === ' resume ') {
+        pauseButton.textContent = ' pause ';
+        timer = setInterval(increment, 1000);
+    }
+}
